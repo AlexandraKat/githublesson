e84b065594d26c3b264e1ebf6ab865998b3c50a3
User <User@erg1-PC07>
2023-11-08 16:00:21 +0200

    fortnite ballsack

---
+++ src/main/java/lesson/github/lesson4/Lesson4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package lesson.github.lesson4;
+
+/**
+ *
+ * @author User
+ */
+public class Lesson4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I SUCK DICK");
+    }
+}
